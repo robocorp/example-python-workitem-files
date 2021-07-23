6f5fbbdfa551e952d21d49460678eee1cd597d6e
Mika Hänninen <mika@robocorp.com>
2021-07-23 10:23:21 +0300

    add method for adding row to csv file

--- task.py
+++ task.py
@@ -12,15 +12,18 @@ def save_file_into_work_item(local_filepath, replace=False):
     items.save_work_item()
 
 
+def add_new_data_row_to_csv_file(filepath, row):
+    data = tables.read_table_from_csv(filepath)
+    tables.add_table_row(data, row)
+    tables.write_table_to_csv(data, filepath)
+
+
 def minimal_task():
-    newdata = [4, "ship"]
     items.load_work_item_from_environment()
     local_data_filepath = items.get_work_item_file(workitem_filename)
     with open(local_data_filepath, "r") as fin:
         print(fin.read())
-    data = tables.read_table_from_csv(local_data_filepath)
-    tables.add_table_row(data, newdata)
-    tables.write_table_to_csv(data, local_data_filepath)
+    add_new_data_row_to_csv_file(local_data_filepath, [4, "ship"])
     # create new workitem file with "updated_" prefix
     save_file_into_work_item(local_data_filepath)
     # remove old workitem file and recreate it with new content
